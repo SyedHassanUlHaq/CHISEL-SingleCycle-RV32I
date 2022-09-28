239f25c0ff818ec6eb6b187e3c63b3331271ac3b
SyedHassanUlHaq <113203732+SyedHassanUlHaq@users.noreply.github.com>
2022-09-29 01:43:59 +0500

    Add files via upload

---
+++ test/scala/TopFileTest.scala
@@ -0,0 +1,14 @@
+package pr
+
+import chisel3._
+import org.scalatest._
+import chiseltest._
+
+class Topfile_Test extends FreeSpec with ChiselScalatestTester{
+    "Top File Test" in {
+        test(new top){
+            C =>
+            C.clock.step(100)
+        }
+    }
+}
